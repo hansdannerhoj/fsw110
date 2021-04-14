6acadb28d341394e80d4a3a2f7121dc44c79c1f1
hansdannerhoj <75225115+hansdannerhoj@users.noreply.github.com>
2021-04-13 20:55:52 -0400

    weekthree

---
+++ week3/index2.js
@@ -0,0 +1,10 @@
+for(i = 0; i < 5; i++){ 
+ var h2 = document.createElement("h2")
+ h2.textContent = "first"
+ document.body.appendChild(h2)
+ h2.style.fontSize ="20px"
+ h2.style.fontWeight = "lighter"
+ h2.style.fontFamily = "sans-serif"
+ h2.style.color = "cornflowerblue"
+ h2.classList = "border"
+}
